63d192df8703e776dc78e8086648fb14ad3cec59
techgeneralista <techgeneralista@gmail.com>
2022-10-29 23:08:56 +0200

    sync

---
+++ WpfAnimatedGif_TabControlIssue/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using WpfAnimatedGif;
+
+namespace WpfAnimatedGif_TabControlIssue
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("pack://application:,,,/Resources/res.gif");
+
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.Default;
+            bitmapImage.UriSource = uri;
+            bitmapImage.EndInit();
+
+            ImageBehavior.SetAnimatedSource(imageControl, bitmapImage);
+
+            // WTF?????
+        }
+    }
+}
