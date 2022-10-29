b7c1dd2c32f9da1c0004fddce95775bcd85f1601
techgeneralista <techgeneralista@gmail.com>
2022-10-29 23:45:01 +0200

    Visibility WTF???

--- WpfAnimatedGif_TabControlIssue/MainWindow.xaml.cs
+++ WpfAnimatedGif_TabControlIssue/MainWindow.xaml.cs
@@ -1,31 +1,25 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media.Imaging;
-using WpfAnimatedGif;
-
 namespace WpfAnimatedGif_TabControlIssue
 {
     public partial class MainWindow : Window
     {
+        public string Src { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
+            DataContext = this;
+
+            Src = "pack://application:,,,/Resources/res.gif";
+
+            // WTF?????
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            var uri = new Uri("pack://application:,,,/Resources/res.gif");
-
-            BitmapImage bitmapImage = new BitmapImage();
-            bitmapImage.BeginInit();
-            bitmapImage.CacheOption = BitmapCacheOption.Default;
-            bitmapImage.UriSource = uri;
-            bitmapImage.EndInit();
-
-            ImageBehavior.SetAnimatedSource(imageControl, bitmapImage);
-
-            // WTF?????
+            
         }
     }
 }
---
+++ WpfAnimatedGif_TabControlIssue/TestControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAnimatedGif;
+
+namespace WpfAnimatedGif_TabControlIssue
+{
+    /// <summary>
+    /// Interaction logic for TestControl.xaml
+    /// </summary>
+    public partial class TestControl : UserControl
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
+                nameof(Source),
+                typeof(Uri),
+                typeof(TestControl),
+                new FrameworkPropertyMetadata(default(Uri), FrameworkPropertyMetadataOptions.None, SourceChanged));
+
+        public Uri Source
+        {
+            get => (Uri)GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+
+        private static void SourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+            => ((TestControl)d).SourceChanged(e);
+
+        private void SourceChanged(DependencyPropertyChangedEventArgs e)
+        {
+            var uri = e.NewValue as Uri;
+
+            if (uri != null)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.Default;
+                bitmapImage.UriSource = uri;
+                bitmapImage.EndInit();
+                ImageBehavior.SetAnimatedSource(imageControl, bitmapImage);
+            }
+        }
+
+        public TestControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
